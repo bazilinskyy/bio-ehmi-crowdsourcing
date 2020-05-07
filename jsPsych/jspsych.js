74d481406d850cd8054dfa6517ea3b01ef1124e1
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2020-05-07 14:25:45 +0300

    Update jspsych.js

--- jsPsych/jspsych.js
+++ jsPsych/jspsych.js
@@ -2390,11 +2390,12 @@ jsPsych.pluginAPI = (function() {
         var preloaded = [];
         for (var i = 0; i < video.length; i++) {
             console.log(video[i], preloaded.includes(video[i]))
-            if preloaded.includes(video[i]) {  // check if video was already preloaded
+            if preloaded.includes(video[i]) == false {  // check if video was already preloaded
               preload_video(video[i]);
-            }
+            } 
             preloaded.push(video[i]);
         }
+        console.log(preloaded)
 
     };
 
