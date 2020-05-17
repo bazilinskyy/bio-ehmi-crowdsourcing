91f3ec87ff3c9c96f1a37ad3a7c992bef2df97b8
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2020-05-17 11:39:48 +0300

    Update jspsych-survey-html-form-ranking.js

--- jsPsych/plugins/jspsych-survey-html-form-ranking.js
+++ jsPsych/plugins/jspsych-survey-html-form-ranking.js
@@ -120,8 +120,8 @@ jsPsych.plugins['survey-html-form-ranking'] = (function() {
     if(trial.unique_values){
       for (var row = 0; row < trial.rows; row++) { // iterate over rows
         for (var item = 0; item < trial.items_per_row; item++) {  // iterate over items in each row
-          console.log('#input' + row + '-' + item);
-          display_element.querySelector('#input' + row + '-' + item).addEventListener('change', function(){
+          console.log('#input' + row + 1 + '-' + item + 1);
+          display_element.querySelector('#input' + row + 1 + '-' + item + 1).addEventListener('change', function(){
             rankings_given[row][item] = display_element.querySelector('#input' + row + '-' + item).value;
             // check if all 4 sliders were moved
             var unique_values = rankings_given[row].filter((item, i, ar) => ar.indexOf(item) === i);
